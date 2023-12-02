0b85ce1e71f348cded64023da884070df854c3cc
Malib100 <belis.marcel@gmail.com>
2023-12-02 11:08:39 +0100

    Added basic Window, MouseListener and KeyListener

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import TypicalGameEngine.Window;
+
+// This is a note to mark the time stamp of the YT video -> 51:43
+
 public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
 }
