c40e31d1881ddff06a7219fe3b9a0b940c71e764
Malib100 <belis.marcel@gmail.com>
2023-12-03 11:37:58 +0100

    Created shaders, renders a colored gradient square

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import TypicalGameEngine.Window;
 
-// This is a note to mark the time stamp of the YT video -> 51:43
+// This is a note to mark the time stamp of the YT video -> 1:11:27
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/TypicalGameEngine/Scene.java
+++ src/main/java/TypicalGameEngine/Scene.java
@@ -6,6 +6,10 @@ public abstract class Scene {
 
     }
 
+    public void init(){
+
+    }
+
     public abstract void update(float dt);
 
 }
